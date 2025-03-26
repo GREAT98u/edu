a57944a067646572e2a2b5e1929ce6f16be8cd51
GREAT98u <anshulgreat987@gmail.com>
2025-03-26 17:55:52 +0530

    json_data

--- user/src/components/Auth.js
+++ user/src/components/Auth.js
@@ -31,9 +31,33 @@ const Auth = () => {
         <div>
             <h2>User Sign-Up</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="name" placeholder="Name" onChange={handleChange} required /><br />
-                <input type="email" name="email" placeholder="Email" onChange={handleChange} required /><br />
-                <input type="password" name="password" placeholder="Password" onChange={handleChange} required /><br />
+                <input type="first_name" name="first_name" placeholder="Name" onChange={handleChange} required /><br />
+                <input type="last_name" name="last_name" placeholder="last_name" onChange={handleChange} required /><br />
+                <input type="email" name="email" placeholder="email" onChange={handleChange} required /><br />
+                <input type="password" name="password" placeholder="password" onChange={handleChange} required /><br />
+                <input type="country" name="country" placeholder="country" onChange={handleChange} required /><br />
+                <input type="state" name="state" placeholder="state" onChange={handleChange} required /><br />
+                <label>Role:</label>
+                <select name="role" onChange={handleChange} required>
+                    <option value="">Select Role</option>  {/* Default placeholder */}
+                    <option value="Student">Student</option>
+                    <option value="Teacher">Teacher</option>
+                    <option value="admin">Admin</option>
+                </select><br />
+                <label>Language:</label>
+                <select name="Language" onChange={handleChange} required>
+                    <option value="">Select Language</option>  {/* Default placeholder */}
+                    <option value="English">English</option>
+                    <option value="Hindi">Hindi</option>
+                    <option value="Tamil">Tamil</option>
+                </select><br />
+                <label>Class:</label>
+                <select name="class" onChange={handleChange} required>
+                    <option value="">Select class</option>  {/* Default placeholder */}
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                </select><br />
                 <button type="submit">Sign Up</button>
             </form>
         </div>
